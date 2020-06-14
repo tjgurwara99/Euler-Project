97d46605446ab558ccfbfe4dd86211936cfb577b
Tajmeet Singh <tjgurwara99@gmail.com>
2020-06-14 02:46:30 +0100

    Documented the 1.cpp

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,9 +1,17 @@
+/**
+ * Copyright 2020 @author tjgurwara99
+ * @file
+ */
 #include <iostream>
 
 using namespace std;
 // This is the limit from the problem
 #define LIMIT 1000
-
+/**
+ * Function to sum the multiples of the given argument to the limit
+ * @param value
+ * @return 'sum' of all the values of less than limit and multiples of parameter value
+ */
 int sum_multiple(int value){
 	int sum = 0;
 	for(int i=value; i<LIMIT; i+=value){
@@ -12,6 +20,9 @@ int sum_multiple(int value){
 	return sum;
 }
 
+/**
+ *  Main function
+ */
 int main() {
 	// This is a simple include exclude principle problem - this optimisation runs the code upto 40% faster than before
     cout << sum_multiple(3) + sum_multiple(5) - sum_multiple(15) << endl;
